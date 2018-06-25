ed5e64615f96ca0a117445635f44e292b95b9fec
= <=>
2018-06-25 14:51:00 +0200

    Updated the player camera to be used with keyboard. q+e to rotate, pgup+pgdown to zoom

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour {
 
-	void Update () {
-        if (Input.GetMouseButton(2)) {
-            RotateCamera();
-        }
-        transform.GetChild(0).position += transform.GetChild(0).forward * Input.mouseScrollDelta.y;
+	private void Update () {
+		RotateCamera();
+		ZoomCamera();
 	}
 
-    void RotateCamera() {
-        transform.Rotate(transform.forward, -Input.GetAxis("Mouse X") * 3);
-    }
+	private void RotateCamera() {
+		float rotation = 0f;
+		if (Input.GetMouseButton(2))
+			rotation -= Input.GetAxis("Mouse X") * 3;
+		if (Input.GetKeyDown(KeyCode.Q))
+			rotation -= 5;
+		if (Input.GetKeyDown(KeyCode.E))
+			rotation += 5;
+		
+		transform.Rotate(transform.forward, rotation);
+	}
+	
+	private void ZoomCamera() {
+		var zoom = Input.mouseScrollDelta.y;
+		if (Input.GetKeyDown(KeyCode.PageUp))
+			zoom += 1f;
+		if (Input.GetKeyDown(KeyCode.PageDown))
+			zoom -= 1f;
+
+		transform.GetChild(0).position += transform.GetChild(0).forward * zoom;
+	}
 }
