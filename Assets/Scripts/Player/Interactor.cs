010b7600d6bf27036e72decee9a877a53c7b0b36
Baste Nesse Buanes <baste@rain-games.com>
2018-06-27 16:53:28 +0200

    Only interact with one interactable

--- Assets/Scripts/Player/Interactor.cs
+++ Assets/Scripts/Player/Interactor.cs
@@ -11,20 +11,30 @@ public class Interactor : MonoBehaviour, IToggelableInputReceiver
         ReceiveInput = true;
     }
 
-    private readonly List<IInteractable> closeInteractables = new List<IInteractable>();
+    private readonly List<IInteractable> interactableBuffer = new List<IInteractable>();
     public void OnUpdate(Inputs inputs)
     {
         if (!inputs.interactDown && !inputs.interactHeld)
             return;
 
-        Physics2DHelper.GetAllNear(transform.position, 1f, -1, closeInteractables);
+        Physics2DHelper.GetAllNear(transform.position, 1f, -1, interactableBuffer);
+        interactableBuffer.Sort(SortInteractables);
+        if (interactableBuffer.Count == 0)
+            return;
 
-        foreach (var interactable in closeInteractables)
-        {
-            if (inputs.interactDown)
-                interactable.OnInteractDown();
-            if (inputs.interactHeld)
-                interactable.OnInteractHeld();
-        }
+        var closest = interactableBuffer[0];
+        if (inputs.interactDown)
+            closest.OnInteractDown();
+        if (inputs.interactHeld)
+            closest.OnInteractHeld();
+    }
+
+    private int SortInteractables(IInteractable x, IInteractable y)
+    {
+        var myPos = transform.position;
+        var xDist = Vector3.SqrMagnitude(((Component) x).transform.position - myPos);
+        var yDist = Vector3.SqrMagnitude(((Component) y).transform.position - myPos);
+
+        return xDist.CompareTo(yDist);
     }
 }
