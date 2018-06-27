ba82974b2a635b3e53a9784cce558271a0656445
spikar1 <Steffen.fureli@gmail.com>
2018-06-27 16:51:03 +0200

    Changed how naming works

--- Assets/Scripts/Ship/Door.cs
+++ Assets/Scripts/Ship/Door.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Door : MonoBehaviour, IInteractable {
 
     public void OnInteract() {
+
         GetComponent<PolygonCollider2D>().isTrigger = !GetComponent<PolygonCollider2D>().isTrigger;
         GetComponent<MeshRenderer>().enabled = !GetComponent<MeshRenderer>().enabled;
     }
